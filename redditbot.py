37210342f6d2bdf08d80ee85c377f9ecb8ee88c1
Zoofru <masonbush752@gmail.com>
2021-11-01 20:15:42 -0500

    comment bot kinda works

--- redditbot.py
+++ redditbot.py
@@ -1,16 +1,61 @@
+import time
+from re import sub
+from praw.reddit import Comment, Submission
+from praw.models import MoreComments
 import requests, shutil, praw, os, datetime, json
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QLabel, QWidget, QPushButton, QVBoxLayout
 
-reddit = praw.Reddit('bot1')
+reddit = praw.Reddit(
+
+)
+
+print(reddit.user.me())
 
 # Made with PyQt5 Designer
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(630, 520)
+        font = QtGui.QFont()
+        font.setPointSize(20)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget_4 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_4.setGeometry(QtCore.QRect(650, 20, 555, 415))
+        self.verticalLayoutWidget_4.setObjectName("verticalLayoutWidget_4")
+        self.label_13 = QtWidgets.QLabel(self.verticalLayoutWidget_4)
+        self.label_13.setObjectName("label_13")
+        self.label_13.setFont(font)
+        self.pushButton_3 = QtWidgets.QPushButton(self.verticalLayoutWidget_4)
+        self.pushButton_3.setEnabled(True)
+        self.pushButton_3.setMinimumSize(QtCore.QSize(200, 50))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.move(0, 250)
+        self.label_14 = QtWidgets.QLabel(self.verticalLayoutWidget_4)
+        self.label_14.setObjectName("label_14")
+        self.label_14.move(0, 50)
+        self.label_15 = QtWidgets.QLabel(self.verticalLayoutWidget_4)
+        self.label_15.setObjectName("label_15")
+        self.label_15.move(0, 100)
+        self.lineEdit_6 = QtWidgets.QLineEdit(self.verticalLayoutWidget_4)
+        self.lineEdit_6.setText("")
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.lineEdit_6.move(75, 97)
+        self.label_16 = QtWidgets.QLabel(self.verticalLayoutWidget_4)
+        self.label_16.setObjectName("label_16")
+        self.label_16.move(0, 130)
+        self.lineEdit_7 = QtWidgets.QLineEdit(self.verticalLayoutWidget_4)
+        self.lineEdit_7.setText("")
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        self.lineEdit_7.move(75, 127)
+        self.label_17 = QtWidgets.QLabel(self.verticalLayoutWidget_4)
+        self.label_17.setObjectName("label_17")
+        self.label_17.move(0, 180)
+        self.textEdit_2 = QtWidgets.QTextEdit(self.verticalLayoutWidget_4)
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.textEdit_2.move(75, 160)
+        self.textEdit_2.resize(150,50)
         self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(20, 70, 254, 414))
         self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
@@ -99,7 +144,6 @@ class Ui_MainWindow(object):
         self.comboBox.addItem(icon, "")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
-        self.comboBox.addItem("")
         self.comboBox.setItemText(3, "")
         self.verticalLayout.addWidget(self.comboBox)
         self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget_3)
@@ -113,6 +157,11 @@ class Ui_MainWindow(object):
         self.line_4.setFrameShape(QtWidgets.QFrame.VLine)
         self.line_4.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line_4.setObjectName("line_4")
+        self.line_5 = QtWidgets.QFrame(self.centralwidget)
+        self.line_5.setGeometry(QtCore.QRect(600, -30, 61, 791))
+        self.line_5.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line_5.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_5.setObjectName("line_5")
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(340, 10, 261, 305))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
@@ -121,8 +170,6 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.setObjectName("verticalLayout_4")
         self.label_7 = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.label_7.setMinimumSize(QtCore.QSize(0, 55))
-        font = QtGui.QFont()
-        font.setPointSize(20)
         self.label_7.setFont(font)
         self.label_7.setObjectName("label_7")
         self.verticalLayout_4.addWidget(self.label_7)
@@ -163,8 +210,6 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.addWidget(self.pushButton_2)
         self.label_12 = QtWidgets.QLabel(self.centralwidget)
         self.label_12.setGeometry(QtCore.QRect(60, 20, 181, 41))
-        font = QtGui.QFont()
-        font.setPointSize(20)
         self.label_12.setFont(font)
         self.label_12.setObjectName("label_12")
         MainWindow.setCentralWidget(self.centralwidget)
@@ -176,13 +221,33 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
         self.pushButton.clicked.connect(self.ok_clicked)
-        self.pushButton_2.clicked.connect(self.run_reply_bot)
+        self.pushButton_2.clicked.connect(self.run_post_bot)
+        self.pushButton_3.clicked.connect(self.comment_bot)
         self.ww = Warning()
     
+    def comment_bot(self):
+        print("Bot is running...")
+        sys.stdout.flush()
+        while True:
+            url = self.lineEdit_6.text()
+            submission = reddit.submission(url=url)
+            for top_level_comment in submission.comments:
+                if isinstance(top_level_comment, MoreComments):
+                    continue
+                if top_level_comment.body == self.lineEdit_7.text():
+                    comment_to_reply = reddit.comment(id=str(top_level_comment.id))
+                    try:
+                        comment_to_reply.reply(self.textEdit_2.toPlainText())
+                    except:
+                        print("Bot cannot reply here!")
+                        sys.stdout.flush()
+                time.sleep(2)
+
+
     def ok_clicked(self):
         self.ww.show()
 
-    def run_reply_bot(self):
+    def run_post_bot(self):
         questions = self.textEdit.toPlainText().split(".")
         print("Bot is running...")
         sys.stdout.flush()
@@ -226,7 +291,13 @@ class Ui_MainWindow(object):
         self.label_10.setText(_translate("MainWindow", "Seperate phrases by a period no spaces.\nex: Who are you.Where are you.What are you"))
         self.label_11.setText(_translate("MainWindow", "Enter Bots Respone"))
         self.pushButton_2.setText(_translate("MainWindow", "Run"))
+        self.pushButton_3.setText(_translate("MainWindow", "Run"))
         self.label_12.setText(_translate("MainWindow", "Info Grabber"))
+        self.label_13.setText(_translate("MainWindow", "Comment Bot"))
+        self.label_14.setText(_translate("MainWindow", "This bot will run through the post at the url submitted.\nIt will then search for your provided phrase and if found\nrespons with your submitted response."))
+        self.label_15.setText(_translate("MainWindow", "Subreddit URL"))
+        self.label_16.setText(_translate("MainWindow", "Trigger"))
+        self.label_17.setText(_translate("MainWindow", "Bots Response"))
 
 class Warning(QWidget):
     def __init__(self):
@@ -247,14 +318,14 @@ class Warning(QWidget):
         btnCancel.clicked.connect(self.cancel)
 
     def agreed(self):
-        hm = ui.spinBox_2.value()
         try:
             subreddit = reddit.subreddit(ui.lineEdit.text().lower())
         except:
             print('subreddit dosent exist')
             sys.stdout.flush()
-        filterby = ''
 
+        hm = ui.spinBox_2.value()
+        filterby = ''
         if ui.comboBox.currentText() == "Top":
             filterby = subreddit.top(limit=int(hm))
         elif ui.comboBox.currentText() == "Hot":
@@ -263,15 +334,16 @@ class Warning(QWidget):
             filterby = subreddit.new(limit=int(hm))
 
         if ui.checkBox_7.isChecked() or ui.checkBox_3.isChecked():
-            if not os.path.isdir("./imgs"):
-                os.mkdir("./imgs")
-            for submission in filterby:
-                self.dl_photos(submission, subreddit)
-
+            try:
+                for submission in filterby:
+                    self.dl_media(submission, subreddit)
+            except:
+                print("Subreddit may not exist - ln 346")
+                sys.stdout.flush()
         else:
 
 
-            if not os.path.isdir("./results"):
+            if not os.path.exists("./results"):
                 os.mkdir("./results")
             date = datetime.datetime.now()
             x = str(date).split(" ")
@@ -299,7 +371,8 @@ class Warning(QWidget):
     def cancel(self):
         self.close()
 
-    def dl_photos(self, submission, subreddit):
+    def dl_media(self, submission, subreddit):
+        sys.stdout.flush()
         dte = datetime.datetime.now()
         # MONTH
         formated = dte.strftime("%B")
@@ -307,32 +380,59 @@ class Warning(QWidget):
         formatedms = dte.strftime("%f")
         # YEAR/WEEKDAY/WEEKNUMBER
         formateddate = dte.strftime("%G%u%V")
-        path = f"./imgs/{formated}/{formateddate}_{subreddit}"
-        if not os.path.isdir(path):
-            os.mkdir(path)
-
-        fp = f"{path}/{formateddate}_{formatedms}.jpg"
-        fpv = f"{path}/{formateddate}_{formatedms}.mp4"
+        path_media = f"./imgs/{formated}/{formateddate}_{subreddit}"
+        if not os.path.exists(path_media):
+            os.makedirs(path_media)
 
         # PHOTOS
         if ui.checkBox_3.isChecked():
-            r = requests.get(submission.url, stream=True)
-            with open(fp, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
+            extension = self.ft_img(submission.url[-3:])
+            try:
+                if not extension:
+                    raise Exception("NoMedia")
+                fp = f"{path_media}/{formateddate}_{formatedms}.{extension}"
+                r = requests.get(submission.url, stream=True)
+                with open(fp, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+            except:
+                print("No Media")
 
         # VIDEOS
         if ui.checkBox_7.isChecked():
             try:
                 # GET JSON TO ACCESS FALLBACK URLS FOR VIDEO
+                # NOT ALL VIDEOS HAVE A FALLBACK URL, THIS IS THE ONLY WAY IVE FIGURED TO GET THE MP4 OF A POST
                 url = reddit.config.reddit_url + submission.permalink + ".json"
-                data = requests.get(url, headers={"User-Agent":"Win64:post_scraper_forstudy:v0.5:/u/randodude13372"}, stream=True).json()
+                data = requests.get(url, , stream=True).json()
                 vUrl = data[0]["data"]["children"][0]["data"]["preview"]["reddit_video_preview"]["fallback_url"]
-                r = requests.get(vUrl, headers={"User-Agent":"Win64:post_scraper_forstudy:v0.5:/u/randodude13372"}, stream=True)
+                extension = self.ft_videos(vUrl[-3:])
+                fpv = f"{path_media}/{formateddate}_{formatedms}.{extension}"
+                r = requests.get(vUrl, , stream=True)
                 with open(fpv, 'wb') as f:
                     shutil.copyfileobj(r.raw, f)
-            except KeyError or UnicodeDecodeError:
-                print(data)
+            except KeyError or UnicodeDecodeError:           
                 print('No Key FALLBACK_URL for video')
+                sys.stdout.flush()
+
+    def ft_img(self, ft):
+        match ft:
+            case "jpg":
+                return "jpg"
+            case _:
+                print("Not Image")
+                sys.stdout.flush()
+                return False
+
+    def ft_videos(self, ft):
+        match ft:
+            case "mp4":
+                return "mp4"
+            case "ifv":
+                return "gifv"
+            case "gif":
+                return "gif"
+            case _:
+                print("Not available format")
                 sys.stdout.flush()
 
 
@@ -342,7 +442,7 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    MainWindow.setFixedSize(650, 530)
+    MainWindow.setFixedSize(950, 530)
     MainWindow.setWindowIcon(QtGui.QIcon('./icon.png'))
     MainWindow.show()
     sys.exit(app.exec_())
